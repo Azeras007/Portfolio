4e87baf9979b11c1eeee69a9a4cb12f54f6643a5
Antoine Laurans <antoine.laurans@epitech.eu>
2022-12-01 14:58:59 +0100

    finish

---
+++ pages/project.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="mainBox">
+        <p>Project</p>
+    </div>
+</template>
+
+<style scoped>
+
+.mainBox {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+}
+a {
+    text-decoration: underline;
+}
+</style>
